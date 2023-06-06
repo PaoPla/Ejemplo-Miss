e212d1318389173545566940e385dc48daa08c7c
PaoPla <131408272+PaoPla@users.noreply.github.com>
2023-06-05 19:40:59 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,13 +13,16 @@ import numpy as np
 
 st.title('Police Incident Reports form 1028 to 2020 in San Francisco')
 
-df = pd.read_csv('https://drive.google.com/file/d/11oLcKiW8SgCOp3tGiQCYuRG7pLL_J-Zf/view?usp=drive_link')
+df = pd.read_csv('Police_Department_Incident_Reports__2018_to_Present.csv')
 
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution')
 
-mapa=pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
 
+Latitude=df['Latitude']
+Longitude=df['Longitude']
+mapa = pd.DataFrame(
+    np.array([[Latitude, Longitude]]),
+    columns=['lat', 'lon']
+)
 mapa = mapa.dropna()
 st.map(mapa.astype(float))
