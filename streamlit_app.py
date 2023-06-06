a493e658a630085704f52dbb2edf699c879d8a11
PaoPla <131408272+PaoPla@users.noreply.github.com>
2023-06-05 19:11:21 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,14 +17,9 @@ df = pd.read_csv('https://drive.google.com/file/d/11oLcKiW8SgCOp3tGiQCYuRG7pLL_J
 
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution')
 
-# Coordenadas geográficas de San Francisco
-latitude = 37.7
-longitude = -122.4 
+mapa=pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
 
-# Crear un DataFrame con las coordenadas de San Francisco
-mapa = pd.DataFrame(
-    np.array([[latitude, longitude]]),
-    columns=['lat', 'lon']
-)
-mapa=mapa.dropna()
+mapa = mapa.dropna()
 st.map(mapa.astype(float))
